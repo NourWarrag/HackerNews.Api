d202a68b46e1d9f4d52887637e282ae3a87fa2d0
NourWarag <155632874+NourWarag@users.noreply.github.com>
2024-03-13 15:31:21 +0400

    Add project files.

---
+++ WebApplication555/Controllers/HackerNewsController.cs
@@ -0,0 +1,32 @@
+﻿using HackerNews.Api.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HackerNews.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class HackerNewsController : ControllerBase
+{
+    private readonly IHackerNewsService _hackerNewsService;
+
+    public HackerNewsController(IHackerNewsService hackerNewsService)
+    {
+        _hackerNewsService = hackerNewsService;
+    }
+
+    [HttpGet("TopStories")]
+    public async Task<List<StoryDto>> GetHackerNewsAsync([FromQuery] int numberOfStories)
+    {
+        var stories = await _hackerNewsService.GetHackerNewsAsync(numberOfStories);
+        return stories.Select(s => new StoryDto
+        {
+            Title = s.Title,
+            Url = s.Url,
+            PostedBy = s.By,
+            Time = s.Time,
+            Score = s.Score,
+            CommentCount = s.Descendants,
+
+        }).ToList();
+    }
+}
---
+++ WebApplication555/Program.cs
@@ -0,0 +1,58 @@
+using HackerNews.Api.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Polly;
+using Polly.Contrib.WaitAndRetry;
+using Polly.Extensions.Http;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+builder.Services.AddHttpClient<IHackerNewsService, HackerNewsService>(client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["HackerNewsApi"]);
+}).AddPolicyHandler(GetRetryPolicy())
+.AddPolicyHandler(GetRateLimitPolicy());
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+app.MapControllers();
+
+app.Run();
+
+
+
+
+static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+{
+
+    var delay = Backoff.DecorrelatedJitterBackoffV2(medianFirstRetryDelay: TimeSpan.FromSeconds(1), retryCount: 5);
+
+    var retryPolicy = HttpPolicyExtensions.HandleTransientHttpError()
+         .OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.NotFound)
+         .WaitAndRetryAsync(delay);
+
+    return retryPolicy;
+}
+
+static IAsyncPolicy<HttpResponseMessage> GetRateLimitPolicy()
+{
+    return Policy.BulkheadAsync<HttpResponseMessage>(5, 10);
+    
+}
+
